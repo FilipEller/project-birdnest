9197759c7f97b43faafc30d7172f00a22a9b2604
Filip Eller <filip.eller@aalto.fi>
2023-01-13 00:17:11 +0200

    Add ESLint to frontend.

---
+++ birdnest-frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  rules: {
+    '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 1,
+    'react/react-in-jsx-scope': 0,
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+};
